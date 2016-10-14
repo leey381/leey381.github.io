63c315367a9f3a1ac7f7cb3fd09a917a8a7b29ea
Ye Eun Lee <leey381@newschool.edu>
2016-10-14 02:05:24 +0000

    js

---
+++ cssadvanced/scripts.js
@@ -0,0 +1,16 @@
+var age= 16;
+var parentsPresent = false ;
+
+if (age >= 18 || parentsPresent == true) {
+    console.log("Come on in...");
+} else {
+    if (age < 17) {
+        console.log("Come back next year");
+    } else {
+    console.log("Come back when you are 18, or bring your parents.");
+    }
+    
+}
+
+
+
